5ffc7d8d0decc387fc7e3b197e47da629ea71c00
Florian Hagniel <florian.hagniel@advens.fr>
2015-06-29 11:10:25 +0200

    Patched in order to support Django 1.7

--- mongodbforms/documentoptions.py
+++ mongodbforms/documentoptions.py
@@ -4,11 +4,14 @@ from types import MethodType
 
 from django.db.models.fields import FieldDoesNotExist
 from django.utils.text import capfirst
-from django.db.models.options import get_verbose_name
 from django.utils.functional import LazyObject
 from django.conf import settings
 
 from mongoengine.fields import ReferenceField, ListField
+try:
+    from django.db.models.options import get_verbose_name
+except ImportError:
+    from django.utils.text import camel_case_to_spaces as get_verbose_name
 
 
 def patch_document(function, instance, bound=True):
